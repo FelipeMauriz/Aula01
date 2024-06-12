79ad31d01e154ecffcf344680d2d7959fd03ff6a
Felipe Mauriz <felipeamorimferreira@gmail.com>
2024-06-12 19:40:41 -0300

    react

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <Text>Aula de React Native</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "yellow",
+    padding: 4,
+    borderWidth: 4,
+    borderStyle: "solid",
+    borderRadius: 30,
+    borderColor: "blue",
+    width: 200,
+    height: 200,
+  },1
+});
